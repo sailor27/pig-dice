589b983888d671bb7d89aafe9c0844f109fd389a
jin camou and sailor winkelman <jin@>
2016-10-24 20:45:15 -0700

    add js functions to create player objects and roll player 1 die

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,6 @@ Player.prototype.rollDie = function() {
 };
 
 
-
-
-
-
-
 //front end logic
 
 $(document).ready(function(){
@@ -27,9 +22,36 @@ $(document).ready(function(){
   $("dl#p1roll").click(function(event){
     event.preventDefault();
     player1.rollDie();
-    $(".p1roll").text(player1.roll);
+    console.log(player1.roll);
+      if (player1.roll === 1){
+        $(".p1roll").text("bust!");
+        $("#p2roll").show();
+      } else {
+        player1.score = (player1.score + this.roll);
+        $("#p1Score").text(player1.score);
+        //second click
+        $("dl#p1roll").click(function(event){
+          event.preventDefault();
+          player1.rollDie();
+
+          //$("#p1Score").text(player1.score);
+          });
+      }
 
 
+
+
+
+    //
+
+
+
+  /*  $("dl#p2roll").click(function(event){
+      event.preventDefault();
+      player2.rollDie();
+      $(".p2roll").text(player2.roll);
+
+    });*/
 
 
 
